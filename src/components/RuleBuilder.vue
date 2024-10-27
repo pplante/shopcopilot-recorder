57a52ce68f4525a2ad55f9689756b8df99c0f78b
Phil Plante <58743+pplante@users.noreply.github.com>
2024-10-26 23:19:56 -0700

    update json when model changes

--- src/components/RuleBuilder.vue
+++ src/components/RuleBuilder.vue
@@ -68,8 +68,7 @@ const selectedUrls: Ref<UrlRecord[]> = ref([])
 const selectedResources: Ref<ResourceRecord[]> = ref([])
 const newRule: Ref<IScopeFilter | null> = ref(null)
 const newRuleText = ref('')
-const elementActions = reactive(model.value?.elements)
-watch([selectedResources, selectedUrls, elementActions], updateRuleText)
+watch([selectedResources, selectedUrls, model], updateRuleText)
 
 function updateRuleText() {
   const applyTo = selectedUrls.value.map(rec => rec.url)
